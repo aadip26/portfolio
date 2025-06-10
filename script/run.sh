1b6a0ee75572eccea048d87bb6f5e2f3f8608039
aadip26 <aadipawar4940@gmail.com>
2025-06-10 20:26:47 +0530

    new appoarch

--- script/run.sh
+++ script/run.sh
@@ -3,6 +3,8 @@
 # sudo rm -r portfolio
 # unzip artifact.zip -d portfolio
 # cd portfolio
-npm run install
+
+cd /home/ubuntu/portfolio
+npm  i
 pm2 delete all
 pm2 start "npm run start" --name portfolio
